33180d0667bdd45ae029862c4b61bfc5ae8096da
NikPratr <NikPrater@gmail.com>
2024-04-29 19:35:21 -0400

    Began server work

---
+++ server/src/database/albums-seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO albums (title, artist, published, artwork, price) VALUES (
+    'Riot Generation',
+    'The Damaged and Broken',
+    '2008-05-02',
+    '../styles/images/generated-album-art/punk-1.png',
+    14.99
+);
--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE IF NOT EXISTS mock_music_website;
 
-USE mock-music-website;
+USE mock_music_website;
 
 CREATE TABLE IF NOT EXISTS albums (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
+    artist VARCHAR(255) NOT NULL,
     published DATE NOT NULL,
     artwork VARCHAR(255) NOT NULL,
     price DECIMAL(4, 2) NOT NULL
-)
+);
